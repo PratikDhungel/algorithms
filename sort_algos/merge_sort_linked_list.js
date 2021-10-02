73fb0ad8daa5ed0251a209c16df69ac85488a3e1
Pratik Dhungel <dhungel.1996@gmail.com>
2021-10-02 21:44:58 +0545

    Linked List Merge Sort complete

--- sort_algos/merge_sort_linked_list.js
+++ sort_algos/merge_sort_linked_list.js
@@ -18,6 +18,50 @@ const splitList = (linkedList) => {
   return { leftHalf, rightHalf };
 };
 
+const mergeList = (leftHalf, rightHalf) => {
+  const merged = new LinkedList();
+
+  // Add a fake head to simplify code, will be removed later
+  merged.addItem(0);
+  currentHead = merged.head;
+
+  leftHead = leftHalf.head;
+  rightHead = rightHalf.head;
+
+  while (leftHead || rightHead) {
+    // If there are no items in left list, add all items from right node to merged list
+    if (leftHead === null) {
+      currentHead.nextNode = rightHead;
+      rightHead = rightHead.nextNode;
+    }
+    // If there are no items in right list, add all items from left node to merged list
+    else if (rightHead === null) {
+      currentHead.nextNode = leftHead;
+      leftHead = leftHead.nextNode;
+    }
+    // Condition of comparison of items
+    else {
+      // If left item is smaller than right, add it to merged list
+      // Move leftHead to next node
+      if (leftHead.value < rightHead.value) {
+        currentHead.nextNode = leftHead;
+        leftHead = leftHead.nextNode;
+      } else {
+        currentHead.nextNode = rightHead;
+        rightHead = rightHead.nextNode;
+      }
+    }
+    currentHead = currentHead.nextNode;
+  }
+
+  // Remove fake head
+
+  const newHead = merged.head.nextNode;
+  merged.head = newHead;
+
+  return merged;
+};
+
 const mergeSort = (linkedList) => {
   if (linkedList.getSize() <= 1) {
     return linkedList;
@@ -26,9 +70,10 @@ const mergeSort = (linkedList) => {
   const { leftHalf, rightHalf } = splitList(linkedList);
   const sortedLeft = mergeSort(leftHalf);
   const sortedRight = mergeSort(rightHalf);
-  // const sortedList = mergeList(sortedLeft, sortedRight);
 
-  // return sortedList;
+  const sortedList = mergeList(sortedLeft, sortedRight);
+
+  return sortedList;
 };
 
 const LL = new LinkedList();
@@ -36,11 +81,12 @@ const LL = new LinkedList();
 LL.addItem(32);
 LL.addItem(14);
 LL.addItem(11);
-// LL.addItem(973);
-// LL.addItem(89);
-// LL.addItem(35);
-// LL.addItem(531);
-// LL.addItem(22);
-// LL.addItem(48);
+LL.addItem(973);
+LL.addItem(89);
+LL.addItem(35);
+LL.addItem(531);
+LL.addItem(22);
+LL.addItem(48);
 
-mergeSort(LL);
+const sortedList = mergeSort(LL);
+console.log(sortedList.repr());
