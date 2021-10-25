9bcf358a84864a870ca4b8dbf8a579f24942f447
Pratik Dhungel <dhungel.1996@gmail.com>
2021-10-25 12:23:22 +0545

    Binary Tree: Tree min value using recursive DFS

---
+++ data_structures/binary_tree/tree_min_value.js
@@ -0,0 +1,33 @@
+const Node = require('./binary_tree');
+
+// Condition -> Tree contains at least one node
+const treeMinValue = (root) => {
+  // Add Base Case
+  if (root === null) return Infinity;
+
+  const leftValue = treeMinValue(root.left);
+  const rightValue = treeMinValue(root.right);
+
+  return Math.min(root.value, leftValue, rightValue);
+};
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//          3
+//       /    \
+//       11    4
+//      / \     \
+//     4   2     1
+
+console.log(treeMinValue(a));
